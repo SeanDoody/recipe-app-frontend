36cbe5eaeafcb964676204171520bbec29f6e001
sean <sdoody95@gmail.com>
2025-02-08 11:02:37 -0500

    Issue #35

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import {
 } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
 import { MatSelectModule } from '@angular/material/select';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -36,9 +38,11 @@ import { FavoritesService } from './services/favorites/favorites.service';
     BrowserAnimationsModule,
     BrowserModule,
     FormsModule,
+    MatButtonModule,
     MatCheckboxModule,
-    MatInputModule,
     MatFormFieldModule,
+    MatInputModule,
+    MatMenuModule,
     MatSelectModule,
     MatIconModule,
     ReactiveFormsModule,
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { Router, Event, NavigationEnd } from '@angular/router';
-import { HostListener } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -8,41 +6,4 @@ import { HostListener } from '@angular/core';
   styleUrls: ['./header.component.scss'],
   standalone: false,
 })
-export class HeaderComponent implements OnInit {
-  showNav: boolean = false;
-  currentRoute: string = '';
-  innerWidth: number = 0;
-
-  constructor(private router: Router) {}
-
-  ngOnInit(): void {
-    this.innerWidth = window.innerWidth;
-    if (this.innerWidth >= 1024) {
-      this.showNav = true;
-    }
-    this.router.events.subscribe((event: Event) => {
-      if (event instanceof NavigationEnd) {
-        this.currentRoute = event.url;
-        if (this.currentRoute === '/') {
-          this.currentRoute = '/search';
-        }
-      }
-    });
-  }
-
-  @HostListener('window:resize', ['$event'])
-  onResize() {
-    this.innerWidth = window.innerWidth;
-    if (this.innerWidth >= 1024) {
-      this.showNav = true;
-    } else {
-      this.showNav = false;
-    }
-  }
-
-  toggleNav(): void {
-    if (this.innerWidth < 1024) {
-      this.showNav = !this.showNav;
-    }
-  }
-}
+export class HeaderComponent {}

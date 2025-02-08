c708765482c89a6d4d9ac092f86fa2feb93d741e
sean <sdoody95@gmail.com>
2025-02-08 10:20:58 -0500

    Issue #19: update search form to use Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,14 @@
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
@@ -15,19 +21,32 @@ import { SearchResultsComponent } from './components/recipe-search-results/recip
 import { EdamamApiService } from './services/edamam-api/edamam-api.service';
 import { FavoritesService } from './services/favorites/favorites.service';
 
-@NgModule({ declarations: [
-        AppComponent,
-        FavoriteRecipesComponent,
-        HeaderComponent,
-        RecipeDetailComponent,
-        RecipeSearchFormComponent,
-        SearchResultsComponent,
-    ],
-    bootstrap: [AppComponent], imports: [AppRoutingModule,
-        BrowserAnimationsModule,
-        BrowserModule,
-        FormsModule,
-        MatCheckboxModule,
-        MatIconModule,
-        ReactiveFormsModule], providers: [EdamamApiService, FavoritesService, provideHttpClient(withInterceptorsFromDi())] })
+@NgModule({
+  declarations: [
+    AppComponent,
+    FavoriteRecipesComponent,
+    HeaderComponent,
+    RecipeDetailComponent,
+    RecipeSearchFormComponent,
+    SearchResultsComponent,
+  ],
+  bootstrap: [AppComponent],
+  imports: [
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    BrowserModule,
+    FormsModule,
+    MatCheckboxModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatIconModule,
+    ReactiveFormsModule,
+  ],
+  providers: [
+    EdamamApiService,
+    FavoritesService,
+    provideHttpClient(withInterceptorsFromDi()),
+  ],
+})
 export class AppModule {}

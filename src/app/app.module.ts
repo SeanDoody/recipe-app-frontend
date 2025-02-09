d5ab0e1fb5fa3d1da6ec1709099558615d36d023
sean <sdoody95@gmail.com>
2025-02-09 12:53:05 -0500

    Issue #34: combine search components and make reactive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,7 +18,6 @@ import { AppComponent } from './app.component';
 import { FavoriteRecipesComponent } from './components/favorite-recipes/favorite-recipes.component';
 import { HeaderComponent } from './components/header/header.component';
 import { RecipeDetailComponent } from './components/recipe-detail/recipe-detail.component';
-import { RecipeSearchFormComponent } from './components/recipe-search-form/recipe-search-form.component';
 import { SearchResultsComponent } from './components/recipe-search-results/recipe-search-results.component';
 import { EdamamApiService } from './services/edamam-api/edamam-api.service';
 import { FavoritesService } from './services/favorites/favorites.service';
@@ -29,7 +28,6 @@ import { FavoritesService } from './services/favorites/favorites.service';
     FavoriteRecipesComponent,
     HeaderComponent,
     RecipeDetailComponent,
-    RecipeSearchFormComponent,
     SearchResultsComponent,
   ],
   bootstrap: [AppComponent],
--- src/app/components/recipe-search-results/recipe-search-results.component.ts
+++ src/app/components/recipe-search-results/recipe-search-results.component.ts
@@ -1,5 +1,17 @@
-import { Component } from '@angular/core';
-import { Observable, shareReplay } from 'rxjs';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import {
+  BehaviorSubject,
+  combineLatest,
+  map,
+  of,
+  startWith,
+  Subject,
+  switchMap,
+  tap,
+} from 'rxjs';
+import { DishType } from 'src/app/models/dish-type.enum';
+import { RecipeSearchForm } from 'src/app/models/recipe-search-form.interface';
 import { Recipe } from 'src/app/models/recipe.interface';
 import { SearchEvent } from 'src/app/models/search-event.interface';
 import { EdamamApiService } from 'src/app/services/edamam-api/edamam-api.service';
@@ -12,16 +24,76 @@ import { FavoritesService } from 'src/app/services/favorites/favorites.service';
   standalone: false,
 })
 export class SearchResultsComponent {
-  public recipes$: Observable<Recipe[]> | null = null;
+  private edamamApiService = inject(EdamamApiService);
+  private favoritesService = inject(FavoritesService);
 
-  constructor(
-    private edamamApiService: EdamamApiService,
-    private favoritesService: FavoritesService
-  ) {}
+  public readonly DishType = DishType;
 
-  public onSearch(searchEvent: SearchEvent): void {
-    this.recipes$ = this.edamamApiService.getRecipes(searchEvent);
-  }
+  public spinWheel = new BehaviorSubject<boolean>(false);
+
+  public searchForm = new FormGroup<RecipeSearchForm>({
+    keywords: new FormControl(),
+    dishType: new FormControl(),
+    glutenFree: new FormControl(),
+    vegan: new FormControl(),
+    vegetarian: new FormControl(),
+  });
+
+  public formSubmit = new Subject<void>();
+
+  private searchEvent$ = this.formSubmit.pipe(
+    map(() => {
+      const formValue = this.searchForm.value;
+      const dietaryRestrictions = [];
+
+      if (formValue.glutenFree) {
+        dietaryRestrictions.push('gluten-free');
+      }
+      if (formValue.vegan) {
+        dietaryRestrictions.push('vegan');
+      }
+      if (formValue.vegetarian) {
+        dietaryRestrictions.push('vegetarian');
+      }
+
+      // To-Do: replace with custom validator
+      if (
+        formValue.keywords === '' &&
+        formValue.dishType?.length === 0 &&
+        dietaryRestrictions.length === 0
+      ) {
+        alert('At least one criteria must be chosen to search.');
+        return null;
+      } else {
+        this.spinWheel.next(true);
+        const newEvent: SearchEvent = {
+          keywords: formValue.keywords ?? '',
+          dishType: formValue.dishType ?? [],
+          dietaryRestrictions: dietaryRestrictions,
+        };
+        return newEvent;
+      }
+    }),
+    tap(() =>
+      setTimeout(() => {
+        this.spinWheel.next(false);
+      }, 1000),
+    ),
+  );
+
+  public recipes$ = this.searchEvent$.pipe(
+    switchMap((searchEvent) => {
+      if (searchEvent) {
+        return this.edamamApiService.getRecipes(searchEvent);
+      }
+      return of(null);
+    }),
+    startWith(null),
+  );
+
+  public viewModel$ = combineLatest([this.spinWheel, this.recipes$]).pipe(
+    map(([spinWheel, recipes]) => ({ spinWheel, recipes })),
+  );
 
   public isRecipeSaved(apiUri: string): boolean {
     return this.favoritesService.isRecipeSaved(apiUri);

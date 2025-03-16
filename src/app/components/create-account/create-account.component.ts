09cbca8da1318969f4049abfb068c12e6655501f
sean <sdoody95@gmail.com>
2025-03-16 19:48:59 -0400

    Issue #46: add create account component

---
+++ src/app/components/create-account/create-account.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-account',
+  imports: [],
+  templateUrl: './create-account.component.html',
+  styleUrl: './create-account.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class CreateAccountComponent {
+
+}
